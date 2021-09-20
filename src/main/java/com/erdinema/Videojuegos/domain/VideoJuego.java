0caf7036dbc21dbeff6f91a02a53326b2fe557e8
ENeculhueque <erickneculhueque@gmail.com>
2021-09-20 18:40:47 -0300

    Cambios pendientes

--- src/main/java/com/erdinema/Videojuegos/domain/VideoJuego.java
+++ src/main/java/com/erdinema/Videojuegos/domain/VideoJuego.java
@@ -5,16 +5,33 @@
  */
 package com.erdinema.Videojuegos.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  *
  * @author necul
  */
+@Entity
 public class VideoJuego {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
     private String nombre;
     private String descripcion;
     private String imagenURL;
 
-  
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public String getNombre() {
         return nombre;
--- src/main/resources/schema-mariadb.sql
+++ src/main/resources/schema-mariadb.sql
@@ -1,6 +1,6 @@
 create table videojuego (
- id inr primary key auto_increment,
+ id int primary key auto_increment,
 nombre varchar(200) not null,
 descripcion varchar(3000),
-imagen_url(5000)
+imagen_url varchar(5000)
 );
